72ec00fddbe818a1a0b7679fa76b82d86f760a31
krumbs <stephaniecjonkers@outlook.com>
2020-07-01 16:57:39 +0200

    Fixes name bugs.

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
 
 from rest_framework import status
 from rest_framework import viewsets
 
 from . import serializers
 from . import models
-
+from . import permissions
 
 # Create your views here.
 
@@ -137,5 +138,11 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
     serializer_class = serializers.UserProfileSerializer
 
-    # how to retrieve the objects from the DB
+    # How to retrieve the objects from the DB
     queryset = models.UserProfile.objects.all()
+
+    # Set the authentication classes tuple
+    authentication_classes = (TokenAuthentication,)
+
+    # Set the permissions tuple
+    permission_classes = (permissions.UpdateOwnProfile,)
